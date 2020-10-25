7066847913e16342d0ccd0e16e4658b1548aad83
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-10-25 21:10:17 +0530

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import yellowLogo from '../assets/images/grub.live-logo-yellow.svg'
 const Footer = () => {
 
 
   return (
-    <div></div>
+    <footer>
+        <div>
+          <img src={yellowLogo} className='logo'></img>
+        </div>
+        <div classNam='footer-links'>
+          <div className='footer-item hyperlink-invert'>Contact Us</div>
+          <div className='footer-item hyperlink-invert'>Feedback</div>
+          <div className='footer-item hyperlink-invert'>About Us</div>
+        </div>
+        <br></br>
+      
+      <span>Made with ❤ in India</span><br></br>
+      <span>© 2020 Grub.live</span>
+    </footer>
   )
 }
 
