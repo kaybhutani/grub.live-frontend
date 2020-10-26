bc69ce34fddd1042d4a2cbb9821cfb31bf6e28f4
kbhutani0001 <kbhutani0001@users.noreply.github.com>
2020-10-26 20:02:38 +0000

    deploy: ffbd6420ee5378eef0470e165d2d7daa62a88bad

---
+++ precache-manifest.ba2790ac61bae6d710845c16fcd547e5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068d6d4b587e4fbda80cadf683973669",
+    "url": "/grub.live-frontend/index.html"
+  },
+  {
+    "revision": "f2c6a2b74f7c244be093",
+    "url": "/grub.live-frontend/static/css/2.b42b50b6.chunk.css"
+  },
+  {
+    "revision": "97594e4aa0622784a060",
+    "url": "/grub.live-frontend/static/css/main.b4b320b3.chunk.css"
+  },
+  {
+    "revision": "f2c6a2b74f7c244be093",
+    "url": "/grub.live-frontend/static/js/2.8619ccde.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/grub.live-frontend/static/js/2.8619ccde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97594e4aa0622784a060",
+    "url": "/grub.live-frontend/static/js/main.6a49c2f5.chunk.js"
+  },
+  {
+    "revision": "be364b9deb0ab21910aa",
+    "url": "/grub.live-frontend/static/js/runtime-main.3a623d05.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/grub.live-frontend/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "2b33b67fd068651e42654654ceacf444",
+    "url": "/grub.live-frontend/static/media/eos-icons.2b33b67f.svg"
+  },
+  {
+    "revision": "9d0a8fed3ea3435f527909647d61227c",
+    "url": "/grub.live-frontend/static/media/eos-icons.9d0a8fed.woff"
+  },
+  {
+    "revision": "a4eddc447083a6fc88f030fc0781655d",
+    "url": "/grub.live-frontend/static/media/eos-icons.a4eddc44.ttf"
+  },
+  {
+    "revision": "be75b648455870523c5bd5720d1f9d22",
+    "url": "/grub.live-frontend/static/media/eos-icons.be75b648.eot"
+  },
+  {
+    "revision": "d2873f138bf019335dd907afc1cb6941",
+    "url": "/grub.live-frontend/static/media/eos-icons.d2873f13.woff2"
+  },
+  {
+    "revision": "8cd86dd6e30dca230722962f1978eab0",
+    "url": "/grub.live-frontend/static/media/grub.live-logo-yellow.8cd86dd6.svg"
+  },
+  {
+    "revision": "3b6b4ff3a6536854af85f162a5c410f0",
+    "url": "/grub.live-frontend/static/media/grub.live-logo.3b6b4ff3.svg"
+  },
+  {
+    "revision": "a579f5a1969924897c12ade263a3f4d1",
+    "url": "/grub.live-frontend/static/media/mobile-qr-menu.a579f5a1.svg"
+  }
+]);
