e991fbd5778dd94dd75568cc70ad11eab4712258
kbhutani0001 <kbhutani0001@users.noreply.github.com>
2020-10-26 17:35:23 +0000

    deploy: 155758b440e6795ea27615d8636124416ee050bb

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grub.live-frontend/precache-manifest.64b13a40acc51065441f545cd3feab18.js"
+  "/grub.live-frontend/precache-manifest.de4f8fcf37aadebdd7385f3dfe6427df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grub.live-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
