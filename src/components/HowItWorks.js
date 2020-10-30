aece7aae782c3a108766b1811da7f914d1973b2e
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-10-30 14:39:09 +0530

    update content

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Step1 from '../assets/images/HowItWorks/step1.svg'
-import Step2 from '../assets/images/HowItWorks/Step2.png'
+import Step2 from '../assets/images/HowItWorks/step2.svg'
 import Step3 from '../assets/images/HowItWorks/step3.svg'
 // import Step4 from '../assets/images/HowItWorks/Step4.png'
 import Step5 from '../assets/images/HowItWorks/step5.svg'
@@ -10,7 +10,7 @@ const StepRight = (props) =>{
     return(
     <div className="how-it-works-div">
     <div className='textLeft'>
-        <h1>Step {props.no}</h1>
+    <h1>{props.no} {props.head}</h1>
         <p>{props.name}</p>
     </div> 
     <div className="imgRight">
@@ -25,7 +25,7 @@ const StepLeft = (props) =>{
     return(
         <div className="how-it-works-div">
          <div className='textRight'>
-            <h1>Step {props.no}</h1>
+    <h1>{props.no} {props.head}</h1>
             <p>{props.name}</p>
         </div> 
         <div className="imgLeft">
@@ -39,38 +39,40 @@ const StepLeft = (props) =>{
 const HowItWorks = () =>{
 
     return(
-       <div>
+       <div className='container'>
+           <div className='align-center'>
+                <h2>How does it works for Restaurant's ?</h2>
+                <p>Let's see how Restaurant's can adapt Contact-less Menu's in just 4 simple steps!</p>
+           </div>
+           <br></br>
        <StepRight
-        name="Enter Restaurant name and Logo(optional)"
-        no="1"
+        name="Restaurant owners enter the name of Restaurant, upload the logo (if there is any) and your Email ID which you can use to edit these menu's Later! If you are a lazy person like us, you can use the Fill dummy data button to create a beautiful menu with dummy data and just edit it!"
+        head="Name and Logo"
+        no="1."
         imageURL={Step1}
         
          />
 
         <StepLeft 
-         name="Create Food Category. (example: Chinese,South Indian etc.)"
-         no="2"
-         imageURL={Step3}
+            head="Categories and Items"
+            name="Create Food Category (example: Chinese,South Indian etc.) and start Adding items to it (example: French fries, burgers). You also have to add the corresponding price. Price can be text and in any currency. You can also use something like Half: $2.99, Full $3.99"
+            no="2"
+            imageURL={Step2}
 
          />
  
         <StepRight
-        name="Add Items and Price to categories"
+        head="Preview your Menu"
+        name="You can watch your menu change Live in the preview section. Just when you think it's perfect, go ahead and Generate it."
         no="3"
-        imageURL={Step2}
+        imageURL={Step3}
         
          />
 
-        <StepLeft 
-         name="Preview Your Menu and Generate QR code"
-         no="4"
-         imageURL={Step1}
-
-         />
-
-        <StepRight
-        name="Now users simply scan QR code and view your virtual Menu"
-        no="5"
+        <StepLeft
+        head="Generate QR Menu"
+        name="Once finalized, click on Generate QR Menu and wait for the QR code to be generated. You can download this QR code sticker and paste it on the tables, doors or wherever you want. User's have to just open the camera and scan this to see the menu."
+        no="4"
         imageURL={Step5}
         
          />
