0def75cbc79f1c8a9302eae3f4d1c4ad48212668
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-10-30 22:08:27 +0530

    typos

--- src/components/PreviewMenu.js
+++ src/components/PreviewMenu.js
@@ -7,7 +7,7 @@ const PreviewMenu = (props) => {
   return (
     <div className='preview-menu'>
       <h2>Preview Menu <i className='eos-icons'>slideshow</i></h2>
-      <p>Once done, create on Generate QR Menu to create your Virtual Menu.</p>
+      <p>Once done, click on Generate QR Menu to create your Virtual Menu.</p>
       <br></br>
       <PreviewRestaurantMenu restaurantDetails={restaurantDetails} />
     </div>
