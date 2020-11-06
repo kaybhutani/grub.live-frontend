6395fc9626a328f6c6fe66c91faf4d6d6225d322
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-11-07 01:24:59 +0530

    about us and contact

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,10 @@ const About = () => {
 
   return (
     <div className='container'>
-      <h1 style={{textAlign: 'center'}}>About Us</h1>
+      <div style={{textAlign: 'center'}}>
+        <h1>About Us</h1>
+        <p></p>
+      </div>
       {/* <br></br> */}
       <div className='container shadow-box'>
         <h2>Who are we ?</h2>
@@ -16,8 +19,9 @@ const About = () => {
         <p>Many Countries and their state governments are mandating that restaurants turn to disposable menus to help protect restaurant visitors against the spread of COVID-19. Disposable single-use menus are safe but it will cost restaurant owners more money. Therefore here we are with our Virtual Menu, ensuring a safe outlet by avoiding any needless interaction with staff. It's Super Quick, Simple, Free, and Easy to Setup. </p>
         <br></br>
         <h2>How are we different ?</h2>
-        <p>Although there are some solutions to the QR menu, but all of them are paid and they are doing more than it is required! Your restaurant simply need a QR menu for now and we promise to provide it for free forever! </p>
+        <p>Although there are some solutions to the QR menu, but all of them are paid and they are doing more than it is required!<br></br>You neither want to replace waiters with robots or have customers order through app even when they are physically present in the restaurant! Your restaurant simply needs a QR menu for now and we promise to provide it for free forever! </p>
       </div>
+      <br></br>
     </div>
   )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,32 +9,33 @@ const Contact = () => {
 
   
   return (
-    <div className='container'> 
-      <h1>Contact Us!</h1>
-      <p>Email ID: <a rel="noopener noreferrer" target='_blank' href='mailto:grubdotlive@gmail.com'>grubdotlive@gmail.com</a></p>
-      <p>Phone number: <a href='tel:+918802999631'>+91 8802999631</a> or <a href='tel:+919911733450'>+91 9911733450</a></p>
-      <p>Social media: 
-        <a rel="noopener noreferrer" target='_blank' href='https://www.instagram.com/grub_live/'><img className='social-icon' src={instagramIcon} alt='instagram'></img></a>
-        <a rel="noopener noreferrer" target='_blank' href='https://twitter.com/grub_live'><img className='social-icon' src={twitterIcon} alt='twitter'></img></a>
-      </p>
-      <br></br>
-      <h1>Send Feedback</h1>
-      <p>We would love to hear your thoughts, suggestions, concerns or problems with anything so we can improve!</p>
-      <br></br>
-      <div className='shadow-box' style={{maxWidth: '1024px',}}>
-        <p>Title</p>
-        <input onChange={(e) => setSubject(e.target.value)} className='form-input' type='text' placeholder='Example: Bug: QR code not downloading'></input>
+    <div className='container'>
+      <div className='container'>
+        <h1>Contact Us!</h1>
+        <p>Email ID: <a rel="noopener noreferrer" target='_blank' href='mailto:grubdotlive@gmail.com'>grubdotlive@gmail.com</a></p>
+        <p>Phone number: <a href='tel:+918802999631'>+91 8802999631</a> or <a href='tel:+919911733450'>+91 9911733450</a></p>
+        <p>Social media: 
+          <a rel="noopener noreferrer" target='_blank' href='https://www.instagram.com/grub_live/'><img className='social-icon' src={instagramIcon} alt='instagram'></img></a>
+          <a rel="noopener noreferrer" target='_blank' href='https://twitter.com/grub_live'><img className='social-icon' src={twitterIcon} alt='twitter'></img></a>
+        </p>
         <br></br>
-        <p>Description</p>
-        <textarea onChange={(e) => setBody(e.target.value)} style={{minHeight: '128px'}} className='form-input'  placeholder='Example: When clicking on Download QR sticker button, no file is downloaded.'></textarea>
+        <h1>Send Feedback</h1>
+        <p>We would love to hear your thoughts, suggestions, concerns or problems with anything so we can improve!</p>
         <br></br>
+        <div className='shadow-box' style={{maxWidth: '1024px',}}>
+          <p>Title</p>
+          <input onChange={(e) => setSubject(e.target.value)} className='form-input' type='text' placeholder='Example: Bug: QR code not downloading'></input>
+          <br></br>
+          <p>Description</p>
+          <textarea onChange={(e) => setBody(e.target.value)} style={{minHeight: '128px'}} className='form-input'  placeholder='Example: When clicking on Download QR sticker button, no file is downloaded.'></textarea>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <a rel="noopener noreferrer" target='_blank' href={`mailto:grubdotlive@gmail.com?subject=${subject}&body=${body}`} className='black-yellow' style={{textDecoration: 'none', margin: '0'}} >Send Feedback</a>
+        </div>
         <br></br>
-        <br></br>
-        <br></br>
-        <a rel="noopener noreferrer" target='_blank' href={`mailto:grubdotlive@gmail.com?subject=${subject}&body=${body}`} className='black-yellow' style={{textDecoration: 'none', margin: '0'}} >Send Feedback</a>
-      </div>
-      <br></br>
-      
+        </div>    
     </div>
   )
 }
