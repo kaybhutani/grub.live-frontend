2189e110e55428cbdc155bbf738efe325ff60dbb
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-11-25 18:58:48 +0530

    add premium switch

--- src/pages/CreateMenu.js
+++ src/pages/CreateMenu.js
@@ -61,7 +61,16 @@ const getCreateMenuComponent = (()=> {
           <>
             {edit?
             (<><h2>Update Menu</h2>
-            <p>Any changes made here will be updated in your old menu only. You can add, delete or update the categories, items and price. The Restaurant name, logo and email ID cannot be changed for now. You don't have to worry for new QR code , the link and QR remains same.</p><br></br></>):<></>}
+            <p>Any changes made here will be updated in your old menu only. You can add, delete or update the categories, items and price. The Restaurant name, logo and email ID cannot be changed for now. You don't have to worry for new QR code , the link and QR remains same.</p><br></br></>)
+            :<></>}
+            <div className='center-align'>
+              {/* <h1>Create Menu</h1> */}
+              <p style={{fontSize: '1.2em'}}>Creating menu is simple. Just enter your restaurant name, upload Logo and Email ID for future changes.<br></br>That's it. Now you can start adding the Dishes by creating a new category and adding items to it.<br></br><br></br>Not enough? Try our premium version to customize menu, add themes, change font, colors, description to each food and more.</p>
+              <div class="checkbox-switch">
+                <input type="checkbox" />
+              </div>
+              <br></br>
+            </div>
             <EditMenu restaurantDetails={restaurantDetails} setRestaurantDetails={setRestaurantDetails} edit={edit} menuId={menuId} hash={hash}/>
             <PreviewMenu restaurantDetails={restaurantDetails} />
           </>
