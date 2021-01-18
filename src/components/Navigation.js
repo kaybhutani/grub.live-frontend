a0a839fba9ff0c4fed39e12b344afd8e88d669c7
Kartikay Bhutani <kbhutani0001@gmail.com>
2021-01-18 20:10:35 +0530

    add dropdown and fuck the whole fucking code up :D

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import logo from '../assets/images/grub.live-logo.svg'
+import Dropdown from './Dropdown/Dropdown'
 
 
 const Navigation = () => {
@@ -14,13 +15,24 @@ const Navigation = () => {
           <img className='logo' src={logo} alt='Grub.live logo'  />
         </div>
       </Link>
-      <div className='nav-item-right'>
-        <Link to='/how-it-works'><div className='hyperlink nav-item'>How it works?</div></Link>
-        <Link to='/create'>
+      <div className='nav-item-right' style={{margin: '16px 16px 0px 0px'}}>
+        
+        <Dropdown text="Restaurants"> 
+          <Link to='/register/restaurant'><div className='hyperlink'>Register Restaurant</div></Link>
+          <Link to='/create'><div className='hyperlink'>Create Menu</div></Link>
+          <Link to='/how-it-works'><div className='hyperlink nav-item'>How it works?</div></Link>
+        </Dropdown>
+        <Dropdown text="Partners"> 
+          <Link to='/register/partner'><div className='hyperlink'>Sign Up</div></Link>
+          <Link to='/register/restaurant'><div className='hyperlink'>Register Restaurant</div></Link>
+          <Link to='/track'><div className='hyperlink'>Track Deals</div></Link>
+        </Dropdown>
+        
+        {/* <Link to='/create'>
           <button className='black-yellow nav-item'>
             Create Menu
           </button>
-        </Link>
+        </Link> */}
       </div>
       </nav>
     </div>
