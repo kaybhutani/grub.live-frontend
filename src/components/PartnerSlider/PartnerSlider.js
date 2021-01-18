ec34cc6ed5f5e5cd99b69b0bbf7ad669978d555a
Kartikay Bhutani <kbhutani0001@gmail.com>
2021-01-18 19:35:59 +0530

    update partner slider

--- src/components/PartnerSlider/PartnerSlider.js
+++ src/components/PartnerSlider/PartnerSlider.js
@@ -1,26 +1,32 @@
 import React, { useState } from 'react'
 
 import styles from './PartnerSlider.module.scss'
-import  Dominos from '../../assets/images/Partners/dominos.png'
-import  MCD from '../../assets/images/Partners/mcd.png';
-import CCD from '../../assets/images/Partners/ccd.png';
+import  dinersclub from '../../assets/images/Partners/dinersclub.jpeg'
+import  infinitylogo from '../../assets/images/Partners/infinitylogo.png';
+import  raftaarlogo from '../../assets/images/Partners/raftaarlogo.png';
+import  tcrclogo from '../../assets/images/Partners/tc2rc2.png';
+import  telegram from '../../assets/images/Partners/telegram.jpg';
+import  verandalogo from '../../assets/images/Partners/vehrandalogo.png';
+import minipunjab from '../../assets/images/Partners/minipunjab.jpeg';
+import bemisaallogo from '../../assets/images/Partners/bemisaallogo.png';
+
 
 
 
 const PartnerSlider = () => {
   
-  let partnerLists = [Dominos,MCD,CCD,Dominos,MCD]
+  let partnerLists = [raftaarlogo, infinitylogo, verandalogo, telegram, dinersclub, tcrclogo, bemisaallogo, minipunjab]
 
 
   return (
     <div className={styles.PartnerSlider}>
       <div>
         <h3>
-        Trusted by 1000+ users
+        Some of our clients
         </h3>
       </div>
       <div className={styles.parnterList}>
-          {partnerLists.map((parnter,index) =><img src={parnter} className={styles.partnerImg} key={index} /> )}
+          <marquee>{partnerLists.map((parnter,index) =><img src={parnter} className={styles.partnerImg} key={index} /> )}</marquee>
       </div>
     </div>
   )
