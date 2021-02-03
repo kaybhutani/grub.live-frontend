f44c44cfd93b33babba627245a5a17bb60af14aa
Lakshya Khera <lakshya.khera@gmail.com>
2021-02-04 02:30:20 +0530

    hotfix

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -3,7 +3,7 @@ import styles from './Dropdown.module.scss'
 
 const Dropdown = ({text, children}) => {
   return (
-    <div class={styles.dropdown}>
+    <div className={styles.dropdown}>
       <span>{text} <i className='eos-icons'>arrow_drop_down</i></span>
       <div class={styles.dropdownContent}>
         {children}
